d4006e78119e6389f1c2d76471f10b5644748f5d
Sohel <shaikhsohel113441@gmail.com>
2025-05-04 16:47:28 +0530

    Render API added

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,18 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+  
+// });
+
+// export default API;
+
+
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://chat-app-project-rupk.onrender.com/api',
+  
 });
 
 export default API;
